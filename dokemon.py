9029636595c5f6f094c33e3369044b83163a6cf4
Carter France <orkierulz@gmail.com>
2021-11-06 14:05:43 -0500

    descriptions begun

--- dokemon.py
+++ dokemon.py
@@ -1,3 +1,37 @@
+import pygame as py
+import json
+
+with open("dokemon.json") as dokemon_file:
+    data = json.load(dokemon_file)
+
+
+
+
+
 class Dokemon:
-    def __init__(self):
-        self.
+    def __init__(self, name, health, sp_defense, sp_attack, defense, speed):
+        self.name = name
+        self.health = health
+        self.sp_defense = sp_defense
+        self.sp_attack = sp_attack
+        self.defense = defense
+        self.speed = speed
+        
+    
+
+dokemonList = []
+
+for dokemon in data['possibleDokemon']:
+    name = str(dokemon['name'])
+    for stats in dokemon['stats']:
+        health = stats['health']
+        defense = stats['defense']
+        speed = stats['speed']
+        sp_attack = stats['sp. attack']
+        sp_defense = stats['sp. defense']
+        
+        new = Dokemon(name, health, sp_defense, sp_attack, defense, speed)
+    dokemonList.append(new)
+    
+for dokemon in dokemonList:
+    print(dokemon.sp_attack)
